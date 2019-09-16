439c3238e4882f61291ff7bc847f86bacd3d8235
Gavin Clive <gavinclive@gmail.com>
2019-09-16 22:46:54 +0700

    Remove ES translation and translate ID error respond

--- resources/lang/id/pagination.php
+++ resources/lang/id/pagination.php
@@ -11,6 +11,6 @@ return [
     | you want to customize your views to better match your application.
     |
     */
-    'previous' => '&laquo; 上一页',
-    'next'     => '下一页 &raquo;',
+    'previous' => '&laquo; Sebelumnya',
+    'next'     => 'Berikutnya &raquo;',
 ];
