f03142061ceeca05f018abcd8105cd48600497dc
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-09-21 13:17:28 +0700

    Images

--- database/migrations/2019_09_17_160405_create_categories_table.php
+++ database/migrations/2019_09_17_160405_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
-            $table->string('url',512);
+            $table->string('image',512);
             $table->timestamps();
         });
     }
