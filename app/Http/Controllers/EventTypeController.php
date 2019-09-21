87d22b113c9d66d6f320025cc7bfa9617c9d6e34
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-09-21 15:32:49 +0700

    Type model, controller, and api

---
+++ app/Http/Controllers/EventTypeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EventType;
+use Illuminate\Http\Request;
+
+class EventTypeController extends Controller
+{
+    private $eventType;
+
+    public function __construct()
+    {
+        $this->eventType = new EventType();
+    }
+
+    public function index()
+    {
+        if(!$this->eventType->getAllEventTypes())
+        {
+            return response()->json(['success' => false, 500]);
+        }
+        return response()->json(['success' => true, 'result' => $this->eventType->getAllEventTypes()], 200);
+    }
+}
