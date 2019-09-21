4a4089a71526e974331061ba0128f1b28a409530
Richard Adhika <richardadhika@gmail.com>
2019-09-21 14:04:26 +0700

    tambah keterangan nama tabel dan seed table categories

--- database/migrations/2019_09_15_070809_create_users_table.php
+++ database/migrations/2019_09_15_070809_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+// Table untuk user 
 
 class CreateUsersTable extends Migration
 {
