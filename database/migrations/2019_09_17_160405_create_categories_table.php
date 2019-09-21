37beb8400f37feb73cecaf70fa498c1475f46e56
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-09-21 13:13:55 +0700

    Add url to types and ctgs tbls

--- database/migrations/2019_09_17_160405_create_categories_table.php
+++ database/migrations/2019_09_17_160405_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
+            $table->string('url',512);
             $table->timestamps();
         });
     }
