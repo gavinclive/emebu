97f3259cb7a5ea4556725bdce4c8c8ef96c32953
Richard Adhika <richardadhika@gmail.com>
2019-09-29 15:33:15 +0700

    Tambahan table comments dan create events

---
+++ database/migrations/2019_09_29_074020_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+//Table untuk create events
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',100);
+            $table->dateTime('startTime');
+            $table->dateTime('endTime');
+            $table->string('location',100);
+            $table->string('summary',150);
+            $table->text('description');
+            $table->string('image',512);
+            $table->string('type',50);
+            $table->string('category',50);
+            $table->integer('organizerId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
---
+++ database/migrations/2019_09_29_081453_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+//Table untuk comments
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('comments', 150);
+            $table->bigInteger('userId');
+            $table->bigInteger('eventId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
