6560559137d44203f7fbe2e192599adac17fbc1c
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-09-29 15:27:55 +0700

    Add tickets and transacions tables
    yeet

---
+++ database/migrations/2019_09_29_081159_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Doctrine\DBAL\Schema\Constraint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('userId');
+            $table->bigInteger('ticketId');
+            $table->integer('qty');
+            $table->timestamps();
+
+            $table->foreign('userId')->references('id')->on('users');
+            $table->foreign('ticketId')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
