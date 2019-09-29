bc8be163c88111d43c68ef5517e6dc23800cd7af
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-09-29 16:10:21 +0700

    Add tickets and transcations tables (#15)
    * Add types and categories tables and trying not to feck up this time for real.
    
    * Add url to types and ctgs tbls
    
    * Images
    
    * Update types in DatabaseSeeder
    
    * Type model, controller, and api
    
    * Update 2019_09_15_070809_create_users_table.php
    
    * Add tickets and transacions tables
    
    * remove camel case

---
+++ database/migrations/2019_09_29_074228_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 128);
+            $table->string('type', 1);
+            $table->integer('qty');
+            $table->string('price', 16);
+            $table->string('desc', 150);
+            $table->bigInteger('event_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
---
+++ database/migrations/2019_09_29_081159_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Doctrine\DBAL\Schema\Constraint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('ticket_id');
+            $table->integer('qty');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
