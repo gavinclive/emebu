f8f8a247fbd2ffb10252f802c521a47891a71820
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-10-05 00:42:36 +0700

    Added Event and Ticket Controller and Model (#19)

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $table = 'tickets';
+
+    protected $fillable =
+    [
+        'name',
+        'type',
+        'qty',
+        'price',
+        'desc',
+        'event_id'
+    ];
+
+    public function getTicketByEventId($event_id)
+    {
+        return $this->getTicketByEventId($event_id);
+    }
+
+    public function storeTicket($ticketArr)
+    {
+        if($ticketArr){
+            return $this->create($ticketArr);
+        }
+        return false;
+    }
 }
