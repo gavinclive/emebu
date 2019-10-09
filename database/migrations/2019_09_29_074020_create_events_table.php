8011883e50368f656bc256ca58aa32915b4a9aed
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2019-10-09 23:13:45 +0700

    Create reports table and edited users, events, tickets, and comments table.

--- database/migrations/2019_09_29_074020_create_events_table.php
+++ database/migrations/2019_09_29_074020_create_events_table.php
@@ -26,9 +26,12 @@ class CreateEventsTable extends Migration
             $table->string('image',512);
             $table->string('type',50);
             $table->string('category',50);
-            $table->integer('organizer_id');
+            $table->string('image_3d', 1024)->nullable();
+            $table->bigInteger('eo_id');
             $table->string('status', 1)->default('1'); //1 for OK, 2 for cancelled, 3 for under investigation
             $table->timestamps();
+
+            $table->foreign('eo_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2019_10_09_145732_create_reports_table.php
+++ database/migrations/2019_10_09_145732_create_reports_table.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-//Table untuk comments
-
-class CreateCommentsTable extends Migration
+class CreateReportsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,15 +13,18 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('reports', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('comments', 150);
+            $table->bigInteger('event_id');         
+            $table->bigInteger('eo_id');
             $table->bigInteger('user_id');
-            $table->bigInteger('event_id');
+            $table->string('desc', 250);
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('eo_id')->references('id')->on('users');
+
         });
     }
 
@@ -34,6 +35,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('reports');
     }
 }
