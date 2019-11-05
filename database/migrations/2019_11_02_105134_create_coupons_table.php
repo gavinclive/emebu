2d5e78db6be3774b599affacdf8121345f4c1546
Gavin Clive <wb-gavincliv532089@antfin.com>
2019-11-05 10:48:44 +0700

    no message

---
+++ database/migrations/2019_11_02_105134_create_coupons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code', 10);
+            $table->integer('qty');
+            $table->string('daily_reset', 1);
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->smallInteger('rate');
+            $table->string('max_cut', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
