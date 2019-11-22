b5437d4404cfe58019ec91533624a68ebabc0230
Gavin Clive <wb-gavincliv532089@antfin.com>
2019-11-22 15:53:06 +0700

    no message

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements JWTSubject //, MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'status', 'last_login'
+        'name', 'email', 'role', 'password', 'status', 'last_login'
     ];
 
     /**
