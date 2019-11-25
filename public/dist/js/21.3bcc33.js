b7295ebd205eea90e9f0603e8289be81b7dbe33f
Gavin Clive <gavinclive@gmail.com>
2019-11-25 20:26:52 +0700

    Segaala jiwa hanya untukmu. tak kan mungkin ada yg lain di sisi ku ingin kau di sini tepiskan sepiku bersamamu...

--- public/dist/js/21.3bcc33.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{77:function(t,e,n){"use strict";n.r(e);var i={middleware:"auth",metaInfo:function(){return{title:this.$t("home")}}},s=n(1),u=Object(s.a)(i,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("\n  "+this._s(this.$t("you_are_logged_in"))+"\n")])}),[],!1,null,null,null);e.default=u.exports}}]);
