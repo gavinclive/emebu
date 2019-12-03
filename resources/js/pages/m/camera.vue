e811fc6461d50bbc5ab2ee7e5d5a2052eea8abec
Gavin Clive <wb-gavincliv532089@antfin.com>
2019-12-03 15:53:14 +0700

    no message

--- resources/js/pages/m/camera.vue
+++ resources/js/pages/m/camera.vue
@@ -3,6 +3,7 @@
     <qrcode-stream @decode="onDecode" class="scanner">
       You can scan attendees ticket here.
     </qrcode-stream>
+    <p class="error">{{ error }}</p>
   </div>
 </template>
 
@@ -14,9 +15,32 @@ export default {
     QrcodeStream
   },
 
+  data: () => ({
+    error: ''
+  }),
+
   methods: {
     onDecode (decodedString) {
       window.location.href = decodedString
+    },
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permisson"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = "ERROR: Stream API is not supported in this browser"
+        }
+      }
     }
   }
 }
